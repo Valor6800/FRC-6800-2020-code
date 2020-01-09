1c694f0bf34e17dc08baf1350b3ae08738b37828
rohitchawla28 <rohitchawla77@gmail.com>
2020-01-08 19:12:26 -0600

    same as the last commit except Rohit remembered to save

---
+++ src/main/include/commands/DriveManual.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <frc2/command/CommandBase.h>
+#include <frc2/command/CommandHelper.h>
+
+#include "subsystems/Drivetrain.h"
+
+/**
+ * An example command that uses an example subsystem.
+ *
+ * <p>Note that this extends CommandHelper, rather extending CommandBase
+ * directly; this is crucially important, or else the decorator functions in
+ * Command will *not* work!
+ */
+class DriveManual : public frc2::CommandHelper<frc2::CommandBase, DriveManual> {
+ public:
+
+    explicit DriveManual(Drivetrain* drivetrain);
+    void Initialize() override;
+    void Execute() override;
+    bool IsFinished() override;
+    void End() override;
+    void Interrupted() override;
+
+ private:
+
+    Drivetrain* m_drivetrain;
+};
