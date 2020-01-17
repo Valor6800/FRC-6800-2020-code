0e95f22811e8cf13bd3d5d2228104aa9cce6262e
rohitchawla28 <rohitchawla77@gmail.com>
2020-01-17 17:04:22 -0600

    testing drivetrain

--- src/main/include/subsystems/IT.h
+++ src/main/include/subsystems/IT.h
@@ -1,30 +1,30 @@
-#pragma once
+// #pragma once
 
-#include <frc2/command/SubsystemBase.h>
-#include <frc/VictorSP.h>
-#include <frc/PWMVictorSPX.h>
+// #include <frc2/command/SubsystemBase.h>
+// #include <frc/VictorSP.h>
+// #include <frc/PWMVictorSPX.h>
 
-#include "Constants.h"
+// #include "Constants.h"
 
-class IT : public frc2::SubsystemBase {
- public:
+// class IT : public frc2::SubsystemBase {
+//  public:
 
-    IT();
+//     IT();
 
-    void SetIntakePower(double power);
-    void SetThroatPower(double power);
+//     void SetIntakePower(double power);
+//     void SetThroatPower(double power);
 
-    void Periodic() override;
+//     void Periodic() override;
 
- private:
+//  private:
 
-    frc::VictorSP intake;
-    frc::VictorSP throatA;
-    frc::VictorSP throatB;
+//     frc::VictorSP intake;
+//     frc::VictorSP throatA;
+//     frc::VictorSP throatB;
 
-};
+// };
 
-// // 7 victor sp
-// // 3 talon on board sped control
-// // 6 canspark
+// // // 7 victor sp
+// // // 3 talon on board sped control
+// // // 6 canspark
 
--- src/main/include/subsystems/Shooter.h
+++ src/main/include/subsystems/Shooter.h
@@ -1,8 +1,9 @@
 #pragma once
 
 #include <frc2/command/SubsystemBase.h>
-#include <frc/VictorSP.h>
-#include <frc/Servo.h>
+// #include <frc/VictorSP.h>
+#include <frc/PWMVictorSPX.h>
+//#include <frc/Servo.h>
 #include "Constants.h"
 
 class Shooter : public frc2::SubsystemBase {
@@ -11,14 +12,14 @@ class Shooter : public frc2::SubsystemBase {
     Shooter();
 
     void SetShooterPower(double power);
-    void TiltHood(double pos);
+    //void TiltHood(double pos);
     void Periodic() override;
 
  private:
 
    // need to add encoder?
    // check controller type
-    frc::VictorSP m_shooter;
-    frc::Servo hood;
+    frc::PWMVictorSPX m_flyWheel;
+   // frc::Servo hood;
     
 };
