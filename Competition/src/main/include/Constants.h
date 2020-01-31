4ffd4f0e2eaee0b120f3a328885ea00335422566
unknown <genevabrown.tx@gmail.com>
2020-01-31 16:31:58 -0600

    Temporary Geneva stuff

--- Competition/src/main/include/Constants.h
+++ Competition/src/main/include/Constants.h
@@ -1,46 +1,72 @@
 #pragma once
 
-/**
- * The Constants header provides a convenient place for teams to hold robot-wide
- * numerical or boolean constants.  This should not be used for any other
- * purpose.
- *
- * It is generally a good idea to place constants into subsystem- or
- * command-specific namespaces within this header, which can then be used where
- * they are needed.
- */
-
-//GAMEPAD CONTROLS
+namespace OIConstants {
 constexpr static int GAMEPAD_BASE_LOCATION = 1;
 constexpr static int GAMEPAD_OPERATOR_LOCATION = 0;
+}
 
-//DRIVETRAIN SUBSYSTEM
+namespace DriveConstants {
 // 1/2/3, 4/5/6 from pdp increasing towards intake
-constexpr static int CAN_ID_LEFT_A = 1;
-constexpr static int CAN_ID_LEFT_B = 2;
-constexpr static int CAN_ID_LEFT_C = 3;
-constexpr static int CAN_ID_RIGHT_A = 4;
-constexpr static int CAN_ID_RIGHT_B = 5;
-constexpr static int CAN_ID_RIGHT_C = 6;
+constexpr static int CAN_ID_LEFT_LEAD = 1;
+constexpr static int CAN_ID_LEFT_FOLLOW_A = 2;
+constexpr static int CAN_ID_LEFT_FOLLOW_B = 3;
+constexpr static int CAN_ID_RIGHT_LEAD = 4;
+constexpr static int CAN_ID_RIGHT_FOLLOW_A = 5;
+constexpr static int CAN_ID_RIGHT_FOLLOW_B = 6;
 
-// ARM SUBSYSTEM
+constexpr auto kTrackwidth = 0.68_m;
+
+constexpr double kWheelDiameterInches = 6;
+constexpr double kGearRatio = 8.8;
+}
+
+namespace RamseteConstants {
+extern const frc::DifferentialDriveKinematics kDriveKinematics;
+
+constexpr auto kMaxSpeed = 4_mps;
+constexpr auto kMaxAcceleration = 4_mps_sq;
+
+constexpr double kRamseteB = 2;
+constexpr double kRamseteZeta = 0.7;
+
+// Convert rotations to meters using gear ratio and wheel diameter (converted to meters)
+constexpr double kPositionConversionFactor = kWheelDiameterInches * 0.0254 / kGearRatio * M_PI;
+// Convert rotations per minute to meters per second 
+constexpr double kVelocityConversionFactor = kPositionConversionFactor / 60;
+
+constexpr bool kGyroReversed = true;
+
+constexpr auto ks = 0.146_V;
+constexpr auto kv = 2.25 * 1_V * 1_s / 1_m;
+constexpr auto ka = 0.315 * 1_V * 1_s * 1_s / 1_m;
+
+constexpr double kPDriveVel = 5.53;
+}
+
+namespace ArmConstants {
 constexpr static int CAN_ID_LEFT_ARM = 7;
 constexpr static int CAN_ID_RIGHT_ARM = 8;
+}
 
-//SHOOTER SUBSYSTEM
+namespace ShooterConstants {
 constexpr static int VICTOR_ID_SHOOTER = 0;
+}
 
-// INTAKE SUBSYSTEM
+namespace IntakeConstants {
 constexpr static int VICTOR_ID_INTAKE = 4;
+}
 
-// HOPPER SUBSYSTEM
+namespace HopperConstants {
 constexpr static int VICTOR_ID_HOPPERA = 2;
 constexpr static int VICTOR_ID_HOPPERB = 8;
+}
 
-// LIFT SUBSYSTEM
+namespace LiftConstants {
 constexpr static int VICTOR_ID_LIFTA = 7;
 constexpr static int VICTOR_ID_LIFTB = 8;
+}
 
-// MUNCHER SUBSYSTEM
+namespace MuncherConstants {
 constexpr static int VICTOR_ID_LEFT_MUNCHER = 9;
 constexpr static int VICTOR_ID_RIGHT_MUNCHER = 10;
+}
--- Competition/src/main/include/subsystems/Drivetrain.h
+++ Competition/src/main/include/subsystems/Drivetrain.h
@@ -8,6 +8,19 @@
 
 #include "Constants.h"
 
+#define kP 0
+#define kI 0
+#define kD 0
+#define kIz 0
+#define kFF 1.0/MaxRPM
+#define MaxRPM 5700
+#define kMaxOutput 1.0
+#define kMinOutput -1.0
+#define kMaxVel 1000
+#define kMinVel 0
+#define kMaxAccel 1500
+#define kAllError 0
+
 class Drivetrain : public frc2::SubsystemBase{
  public:
 
@@ -17,19 +30,31 @@ class Drivetrain : public frc2::SubsystemBase{
 
     void Move(double leftInput, double rightInput);
 
+    void InitDrives();
+
     void Stop();
+
+    double boostMultiplier;
+
+    void SetMultiplier(double multiplier);
 
  private:
 
-    rev::CANSparkMax leftDriveA;
-    rev::CANSparkMax leftDriveB;
-    rev::CANSparkMax leftDriveC;
-    rev::CANSparkMax rightDriveA;
-    rev::CANSparkMax rightDriveB;
-    rev::CANSparkMax rightDriveC;
+    rev::CANSparkMax m_leftDriveLead;
+    rev::CANSparkMax m_leftDriveFollowA;
+    rev::CANSparkMax m_leftDriveFollowB;
+    rev::CANSparkMax m_rightDriveLead;
+    rev::CANSparkMax m_rightDriveFollowA;
+    rev::CANSparkMax m_rightDriveFollowB;
 
-    frc::SpeedControllerGroup leftDrive{leftDriveA, leftDriveB, leftDriveC};
-    frc::SpeedControllerGroup rightDrive{rightDriveA, rightDriveB, rightDriveC};
+    rev::CANPIDController m_leftPIDController = m_leftDriveLead.GetPIDController();
+    rev::CANPIDController m_rightPIDController = m_rightDriveLead.GetPIDController();
+
+    rev::CANEncoder m_leftEncoder = m_leftDriveLead.GetEncoder();
+    rev::CANEncoder m_rightEncoder = m_rightDriveLead.GetEncoder();
+
+    frc::SpeedControllerGroup leftDrive{m_leftDriveLead, m_leftDriveFollowA, m_leftDriveFollowB};
+    frc::SpeedControllerGroup rightDrive{m_rightDriveLead, m_rightDriveFollowA, m_rightDriveFollowB};
 
     frc::DifferentialDrive drive{leftDrive, rightDrive};
 
