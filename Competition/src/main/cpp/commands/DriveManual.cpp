f32e833743fe4326957412360a22bcaa805100f3
unknown <genevabrown.tx@gmail.com>
2020-01-31 21:02:52 -0600

    working rocket league controls in command based

--- Competition/src/main/cpp/commands/DriveManual.cpp
+++ Competition/src/main/cpp/commands/DriveManual.cpp
@@ -1,14 +1,14 @@
 #include "commands/DriveManual.h"
 
-DriveManual::DriveManual(Drivetrain* drivetrain, std::function<double()> straightValue, std::function<double()> turnValue) 
-            : m_drivetrain{drivetrain}, m_straightValue{straightValue}, m_turnValue{turnValue} {
+DriveManual::DriveManual(Drivetrain* drivetrain, std::function<double()> straightValue, std::function<double()> reverseValue, std::function<double()> turnValue) 
+            : m_drivetrain{drivetrain}, m_straightValue{straightValue}, m_reverseValue{reverseValue}, m_turnValue{turnValue} {
     AddRequirements({drivetrain});
 
 }
 
 void DriveManual::Execute() {
     //m_drivetrain->ArcadeDrive(m_leftValue(), m_rightValue());
-    m_drivetrain->RocketLeagueDrive(m_straightValue(), m_turnValue());
+    m_drivetrain->RocketLeagueDrive(m_straightValue(), m_reverseValue(), m_turnValue());
 }
 
 bool DriveManual::IsFinished() {
