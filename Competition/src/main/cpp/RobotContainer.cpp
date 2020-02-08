7b717920da2849aa84281ea6ea42700efdb90176
Michael Ray <michael.ray@arm.com>
2020-02-08 16:40:50 -0600

    Used shared pointer stuff

--- Competition/src/main/cpp/RobotContainer.cpp
+++ Competition/src/main/cpp/RobotContainer.cpp
@@ -1,54 +1,40 @@
 #include "RobotContainer.h"
 
+#include "commands/DriveManual.h"
+#include "commands/ShootStart.h"
+#include "commands/SpinIntake.h"
+#include "commands/SpinHopper.h"
+#include "commands/ShootStop.h"
+#include "commands/ArmManual.h"
+#include "commands/Climb.h"
+#include "commands/Munch.h"
 
-RobotContainer::RobotContainer() : m_homeTrenchAuto(&m_drivetrain) {
-    // Initialize all of your commands and subsystems here
-    
-    m_drivetrain.SetDefaultCommand(DriveManual(&m_drivetrain,
-        [this] { return m_GamepadDriver.GetTriggerAxis(frc::GenericHID::kRightHand); },
-        [this] { return m_GamepadDriver.GetTriggerAxis(frc::GenericHID::kLeftHand); },
-        [this] { return m_GamepadDriver.GetX(frc::GenericHID::kLeftHand); },
-        [this] { return m_GamepadDriver.GetBumper(frc::GenericHID::kLeftHand); }));
-
-    m_intake.SetDefaultCommand(SpinIntake(&m_intake, 
-        [this] { return m_GamepadDriver.GetTriggerAxis(frc::GenericHID::kRightHand); }, 
-        [this] { return m_GamepadDriver.GetBumper(frc::GenericHID::kRightHand); }));
-            
-    m_hopper.SetDefaultCommand(SpinHopper(&m_hopper, 
-        [this] { return m_GamepadDriver.GetTriggerAxis(frc::GenericHID::kLeftHand); }, 
-        [this] { return m_GamepadDriver.GetBumper(frc::GenericHID::kLeftHand); }));
-
-    m_arm.SetDefaultCommand(ArmManual(&m_arm, 
-        [this] { return m_GamepadDriver.GetY(frc::GenericHID::kLeftHand); }));
-
-    m_lift.SetDefaultCommand(Climb(&m_lift, 
-        [this] { return m_GamepadDriver.GetY(frc::GenericHID::kRightHand); }));
-
-    m_muncher.SetDefaultCommand(Munch(&m_muncher, 
-        [this] { return m_GamepadDriver.GetYButton(); }));
-
+RobotContainer::RobotContainer() {
     // Configure the button bindings
     ConfigureButtonBindings();
 }
 
 void RobotContainer::ConfigureButtonBindings() {
-    frc2::JoystickButton m_start{&m_GamepadDriver, 8};
-    frc2::JoystickButton m_back{&m_GamepadDriver, 7};
+    // Start shooter button (Operator: Start)
+    frc2::JoystickButton operator_start{&m_GamepadOperator, 8};
+    // Stop shooter button (Operator: Back)
+    frc2::JoystickButton operator_back{&m_GamepadOperator, 7};
 
-    // frc::JoystickButton m_a{&m_GamepadDriver, 1};
-    frc2::JoystickButton m_b{&m_GamepadDriver, 2};
-    // frc::JoystickButton m_x{&m_GamepadDriver, 3};
-    //frc2::JoystickButton m_y{&m_GamepadDriver, 4};
-    
-    m_b.WhenPressed(frc2::InstantCommand([this] { m_drivetrain.SetMultiplier(1); }, {&m_drivetrain}));
+    // Driver button (unused) (Driver: A)
+    frc2::JoystickButton driver_a{&m_GamepadDriver, 1};
+    // Driver boost button (Driver: B)
+    frc2::JoystickButton driver_b{&m_GamepadDriver, 2};
+    // Driver button (unused) (Driver: X)
+    frc2::JoystickButton driver_x{&m_GamepadDriver, 3};
+    // Driver button (unused) (Driver: Y)
+    frc2::JoystickButton driver_y{&m_GamepadDriver, 4};
 
-    m_start.WhenPressed(ShootStart(&m_shooter));
-    m_back.WhenPressed(ShootStop(&m_shooter));
-
-
-    
-}
-frc2::Command* RobotContainer::GetAutonomousCommand() {
-    // An example command will be run in autonomous
-    return &m_homeTrenchAuto;
+    // Operator button (unused) (Operator: A)
+    frc2::JoystickButton operator_a{&m_GamepadOperator, 1};
+    // Operator boost button (Operator: B)
+    frc2::JoystickButton operator_b{&m_GamepadOperator, 2};
+    // Operator button (unused) (Operator: X)
+    frc2::JoystickButton operator_x{&m_GamepadOperator, 3};
+    // Operator button (unused) (Operator: Y)
+    frc2::JoystickButton operator_y{&m_GamepadOperator, 4};
 }
