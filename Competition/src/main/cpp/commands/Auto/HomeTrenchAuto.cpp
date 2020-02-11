da4e6182d3033f888cfd59a5e3775767c8814f78
unknown <genevabrown.tx@gmail.com>
2020-02-10 19:52:54 -0600

    close to compile to test auto

--- Competition/src/main/cpp/commands/Auto/HomeTrenchAuto.cpp
+++ Competition/src/main/cpp/commands/Auto/HomeTrenchAuto.cpp
@@ -1,14 +1,14 @@
-#include <commands/Auto/HomeTrenchAuto.h>
+// #include <commands/Auto/HomeTrenchAuto.h>
 
-HomeTrenchAuto::HomeTrenchAuto() : m_kinematics(DriveConstants::kTrackwidth) {
-    frc2::RamseteCommand ramsete_command_(
-        m_trajectory, [&]() { return Drivetrain::GetInstance().GetPose(); },
-        frc::RamseteController(RamseteConstants::kRamseteB, RamseteConstants::kRamseteZeta),
-        frc::SimpleMotorFeedforward<units::meters>(RamseteConstants::kS,RamseteConstants::kV,RamseteConstants::kA),
-        m_kinematics,
-        [&] { return Drivetrain::GetInstance().GetWheelSpeeds(); },
-        frc2::PIDController(RamseteConstants::kPDriveVel, 0, 0),
-        frc2::PIDController(RamseteConstants::kPDriveVel, 0, 0),
-        [&](auto left, auto right) { Drivetrain::GetInstance().TankDriveVolts(left, right); },
-    {&Drivetrain::GetInstance()});
-}
+// HomeTrenchAuto::HomeTrenchAuto() : m_kinematics(DriveConstants::kTrackwidth) {
+//     frc2::RamseteCommand ramsete_command_(
+//         m_trajectory, [&]() { return Drivetrain::GetInstance().GetPose(); },
+//         frc::RamseteController(RamseteConstants::kRamseteB, RamseteConstants::kRamseteZeta),
+//         frc::SimpleMotorFeedforward<units::meters>(RamseteConstants::kS,RamseteConstants::kV,RamseteConstants::kA),
+//         m_kinematics,
+//         [&] { return Drivetrain::GetInstance().GetWheelSpeeds(); },
+//         frc2::PIDController(RamseteConstants::kPDriveVel, 0, 0),
+//         frc2::PIDController(RamseteConstants::kPDriveVel, 0, 0),
+//         [&](auto left, auto right) { Drivetrain::GetInstance().TankDriveVolts(left, right); },
+//     {&Drivetrain::GetInstance()});
+// }
--- Competition/src/main/include/commands/Auto/HomeTrenchAuto.h
+++ Competition/src/main/include/commands/Auto/HomeTrenchAuto.h
@@ -1,19 +1,19 @@
-#pragma once
+// #pragma once
 
-#include <frc2/command/CommandHelper.h>
-#include <frc2/command/SequentialCommandGroup.h>
-#include <frc/kinematics/DifferentialDriveKinematics.h>
-#include <frc/controller/SimpleMotorFeedforward.h>
+// #include <frc2/command/CommandHelper.h>
+// #include <frc2/command/SequentialCommandGroup.h>
+// #include <frc/kinematics/DifferentialDriveKinematics.h>
+// #include <frc/controller/SimpleMotorFeedforward.h>
 
-#include "InitHomeTrench.h"
+// #include "InitHomeTrench.h"
 
-#include "Constants.h"
+// #include "Constants.h"
 
-class HomeTrenchAuto : public frc2::CommandHelper<frc2::SequentialCommandGroup, HomeTrenchAuto> {
- public:
-    HomeTrenchAuto();
+// class HomeTrenchAuto : public frc2::CommandHelper<frc2::SequentialCommandGroup, HomeTrenchAuto> {
+//  public:
+//     HomeTrenchAuto();
 
-private:
-    frc::Trajectory m_trajectory;
-    frc::DifferentialDriveKinematics m_kinematics;
-};
+// private:
+//     frc::Trajectory m_trajectory;
+//     frc::DifferentialDriveKinematics m_kinematics;
+// };
