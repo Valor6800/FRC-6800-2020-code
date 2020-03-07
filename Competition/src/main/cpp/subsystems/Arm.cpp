d690db475e9fb110d4c92a9306e23475863a54c7
rohitchawla28 <rohitchawla77@gmail.com>
2020-03-07 11:04:30 -0600

    arm manual control

--- Competition/src/main/cpp/subsystems/Arm.cpp
+++ Competition/src/main/cpp/subsystems/Arm.cpp
@@ -1,13 +1,21 @@
 #include "subsystems/Arm.h"
 
-Arm::Arm() : leftArmMtr{ArmConstants::TALON_ID_LEFT_ARM}, rightArmMtr{ArmConstants::TALON_ID_RIGHT_ARM} {
+Arm::Arm() : armMtrLeft{ArmConstants::TALON_ID_LEFT_ARM}, 
+             armMtrRight{ArmConstants::TALON_ID_RIGHT_ARM} {
     
 }
 
-Arm& Arm::GetInstance()
-{
-    static Arm instance; // Guaranteed to be destroyed. Instantiated on first use.
+Arm& Arm::GetInstance() {
+    static Arm instance;
     return instance;
+}
+
+void Arm::InitArm() {
+    armMtrLeft.ConfigFactoryDefault();
+    armMtrRight.ConfigFactoryDefault();
+
+    armMtrLeft.SetInverted(false);
+    armMtrRight.SetInverted(false);
 }
 
 void Arm::Periodic() {
@@ -15,5 +23,6 @@ void Arm::Periodic() {
 }
 
 void Arm::SetArmPower(double power) {
-    armMtrs.Set(power);
+    armMtrLeft.Set(ControlMode::PercentOutput, power * 0.25);
+    armMtrRight.Set(ControlMode::PercentOutput, power * 0.25);
 }
